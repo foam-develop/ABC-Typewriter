520a379b6ca8aee899c61056fd2e91dfc6f1217b
Winke Wiegersma <winkewiegersma@gmail.com>
2022-02-02 10:29:57 +0100

    update

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 const log = document.getElementById('log');
-const click = document.getElementById('clickhere');
 const container = document.getElementById('container');
 const input = document.getElementById('input');
 const clear = document.getElementById('clear');
 const logContainer = document.getElementById('log-container');
-document.addEventListener('keydown', logKey);
-var string = [];
+input.addEventListener("input", updateValue);
+var backspace = false;
 var keyTyped = false;
-const alphabet = ["a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z", " "];
-const specialChars = ["Backspace", "Enter"];
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "Backspace", "Enter", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var i = 0;
-var txt = 'you can use lowercase a-z, SPACE, BACKSPACE and ENTER keys to type your message. no numbers or special characters.';
-var speed = 50;
+// var i = 0;
+// var txt = 'you can use lowercase a-z, SPACE, BACKSPACE and ENTER keys to type your message. no numbers or special characters.';
+// var speed = 50;
 
-function typeWriter() {
-    if (i < txt.length) {
-      document.getElementById("key").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-    } else {
-        click.style.display = "block";
-        clear.style.display = "block";
-    }
-  }
+// function typeWriter() {
+//     if (i < txt.length) {
+//       document.getElementById("key").innerHTML += txt.charAt(i);
+//       i++;
+//       setTimeout(typeWriter, speed);
+//     } else {
+//         clear.style.display = "block";
+//     }
+//   }
 
-setTimeout(typeWriter, 1650);
+// setTimeout(typeWriter, 1650);
 
 var j = 0;
 var title = 'ABC da Cana Typewriter';
@@ -41,34 +38,12 @@ function typeWriterTitle() {
 
 typeWriterTitle();
 
-click.onclick = function () { 
-    click.style.display = "none";
-}; 
-
-input.onclick = function () { 
-    input.innerHTML = "";
-}; 
-
 clear.onclick = function () {
     logContainer.innerHTML = "";
     log.innerHTML = "";  
-    input.innerHTML = "";
+    input.value = "";
     string = [];
-    click.style.display = "none";
 }
-
-function checkIfLetter(key) {
-    if (alphabet.includes(key)) {
-        // key = key.toLowerCase();
-        keyTyped = true;
-        console.log(key + keyTyped);
-    } else if (specialChars.includes(key)) {
-        keyTyped = true;
-    } else {
-        keyTyped = false;
-        console.log(key + keyTyped);
-    }
-}   
 
 function addImage(i, container) {
     const image = document.createElement("img");
@@ -84,29 +59,41 @@ function addSpace(s, container) {
     container.appendChild(space);
 }
 
-function logKey(e) {
-    checkIfLetter(e.key);
-    click.style.display = "none";
-    if (keyTyped) {
+input.addEventListener("keydown", function(e) {
+    if (alphabet.includes(e.key)) {
+        keyTyped = true;
         if (e.key === "Backspace") {
-            if (string.length > 0) {
-                string.pop();
-                logContainer.removeChild(logContainer.lastElementChild);
-            }
-        } else if (e.key === " ") {
-            addSpace(e.key, logContainer);
-            string.push(e.key);
-        } else if (e.key === "Enter") {
-            const enter = document.createElement("br");
-            logContainer.appendChild(enter);
-            string.push("<br/>");
+            backspace = true;
         } else {
-            addImage(e.key, logContainer);
-            console.log(string);
-            string.push(e.key);
+            backspace = false;
         }
-        // log.innerHTML = string.join('');
+    } else {
+        keyTyped = false;
+    }
+});
+
+function updateValue(e) {
+    if (keyTyped) {
+        var string = e.target.value.split("");
+        if (string.length >= 0) {
+            var lastKey = string[string.length-1];
+            // console.log("last key is " + lastKey);
+            if (backspace) {
+                logContainer.removeChild(logContainer.lastElementChild);
+            } else if (lastKey === " ") {
+                addSpace(lastKey, logContainer);
+            } else if (lastKey === "\n") {
+                const enter = document.createElement("br");
+                logContainer.appendChild(enter);
+            } else if (alphabet.includes(lastKey)) {
+                addImage(lastKey, logContainer);
+            }
+            console.log(string);
+        }
     }
 }
 
-
+function restrict(x) {
+    x.value = x.value.replace(/[^a-z\s]/i, "");
+    console.log("restrict works");
+}
