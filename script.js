6a0464eb6fd0f282bca67e77a3b0bbeeafc4b389
Winke Foam <foam@MacBook-Air-van-Garance.local>
2022-02-10 17:03:37 +0100

    added hover effect

--- script.js
+++ script.js
@@ -13,6 +13,34 @@ var backspace = false;
 var keyTyped = false;
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "Backspace", "Enter", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+const imgTxts = {
+    a: "All images in this series were taken in the lunch break of the workers from the TABU refinery.",
+    b: "By performing the alphabet, the artist, Jonathas de Andrade is commenting on...",
+    c: "Composition and Colour: each photo is a careful composition of the labourers bending, stretching and manipulating the shape of the sugar cane. Their mostly blue work costumes constrast with the brown-beige background of the sugar cane fields.",
+    d: "Dee",
+    e: "EE",
+    f: "Foam Amsterdam is currently exhibiting Next Level: Jonathas de Andrade <span class='italic'>Staging Resistance</span>.",
+    g: "Gee",
+    h: "HH",
+    i: "Inspiration came from the 1957 typography of Luís Jardim for Brasil Açucareiro, a trade magazine published by the Brazilian Institute for Alcohol and Sugar.",
+    j: "Jonathas de Andrade is a Brazilian artist who works with...",
+    k: "KAa",
+    l: "LL",
+    m: "Mmm",
+    n: "Next Level is an exhibition series that introduces artists who are making radical use of photography. Jonathas de Andrade is the current Next Level artist on display in Foam Photography Museum Amsterdam.",
+    o: "Ooh",
+    p: "Photo challenge: think of type of food that you like or you perhaps currently have at home, and re-create the alphabet using only this type of food. Think carefully about the composition and use of colour: what background do you use so your letters can stand out and be read easily? How do you arrange the letters on this background? Take a photo of each letter and share using #foamabc.",
+    q: "quu",
+    r: "Rrrr",
+    s: "Sugar cane is the raw material for refined sugar. It has been an important Brazilian export product since colonial times, and has a dark history of slavery, land reclamation through logging, and unequal distribution of natural resources.",
+    t: "Tutut",
+    u: "Uuu",
+    v: "Visit us Next Level: Jonathas de Andrade <span class='italic'>Staging Resistance</span> at Foam Photography Museum Amsterdam (Keizersgracht 609, Amsterdam).",
+    w: "What stands out in this series, is the composition and use of colour. Each photo is a careful composition of the labourers bending, stretching and manipulating the shape of the sugar cane. Their mostly blue work costumes constrast with the brown-beige background of the sugar cane fields.",
+    x: "Xxx",
+    y: "Y",
+    z: "Zz",
+}
 // var i = 0;
 // var txt = 'you can use lowercase a-z, SPACE, BACKSPACE and ENTER keys to type your message. no numbers or special characters.';
 // var speed = 50;
@@ -50,23 +78,15 @@ clear.onclick = function () {
     string = [];
 }
 
-
 function toggle(id) {
     navTxts.forEach(navtxt => navtxt.style.display = "none");
     document.getElementById(id + "-txt").style.display = "block";
 }
-// about.onclick = function () {
-//     aboutTxt.style.display = "block";
-// }
-
-// key.onclick = function () {
-//     keyTxt.style.display = "block";
-// }
 
 function addImage(i, container) {
     const image = document.createElement("img");
     image.setAttribute("src", "img/" + i + ".jpg");
-    image.setAttribute("class", "img");
+    image.setAttribute("class", "img " + i);
     container.appendChild(image);
     return image;
 }
@@ -105,6 +125,7 @@ function updateValue(e) {
                 logContainer.appendChild(enter);
             } else if (alphabet.includes(lastKey)) {
                 addImage(lastKey, logContainer);
+                hover(lastKey);
             }
             console.log(string);
         }
@@ -114,4 +135,15 @@ function updateValue(e) {
 function restrict(x) {
     x.value = x.value.replace(/[^a-z\s]/i, "");
     console.log("restrict works");
+}
+
+function hover(input) {
+    document.querySelectorAll("." + input).forEach(image => image.addEventListener("mouseover", function() {
+        toggle("img");
+        var txt = imgTxts[input];
+        document.getElementById("img-txt").innerHTML = txt;
+    }));
+    document.querySelectorAll("." + input).forEach(image => image.addEventListener("mouseout", function() {
+        document.getElementById("img-txt").innerHTML = "";
+    }));
 }
